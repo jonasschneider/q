926ad3ca8718dd49432d6b4ad771a5d37ef38452
Jonas Schneider <mail@jonasschneider.com>
2015-03-21 19:50:43 +0100

    clean up rx.ino

--- console/rx/rx.ino
+++ console/rx/rx.ino
@@ -1,45 +1,44 @@
-
 #include "VirtualWire.h"
 
 void setup()
 {
-    Serial.begin(9600);	  // Debugging only
+    Serial.begin(9600);   // Debugging only
     Serial.println("setup");
     delay(1);
 
-    // Initialise the IO and ISR
-//    vw_set_tx_pin(8);
     pinMode(13, OUTPUT);
     vw_set_rx_pin(3);
-//    vw_set_ptt_inverted(true); // Required for DR3100
-//    vw_setup(8*8);	 // Bits per sec
-// for arduino
-    vw_setup(122*2);
-    vw_rx_start();
-    
 
+    // with STM32 TX:
+    vw_setup(8*8);
+
+    // for Arduino TX
+    // The TX's timer0 fires (16 000 000 / 64) / 256 =~ 976 times per second.
+    // Since we need 8 timer firings to transfer a single bit, divide by 8.
+    // TODO: fix the 2 factor by removing nticks*2 from the implementation.
+//    vw_setup(122*2);
+
+    vw_rx_start();
 }
 
 void loop()
 {
     vw_wait_rx();
-    
+
     uint8_t buf[100];
     uint8_t buflen = 100;
     if (vw_get_message(buf, &buflen)) // Non-blocking
     {
-	int i;
-	// Message with a good checksum received, dump it.
-	Serial.print("\nGot data:");
+        int i;
+        Serial.print("\nGot data:");
         Serial.print(buflen, DEC);
         Serial.print(":\n");
-	
-	for (i = 0; i < buflen; i++)
-	{
-            char c = (buf[i]);
-            Serial.print(c);
-	}
-	Serial.println("\n");
+
+        for (i = 0; i < buflen; i++)
+        {
+                char c = (buf[i]);
+                Serial.print(c);
+        }
+        Serial.println("\n");
     }
 }
-
