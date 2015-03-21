8187b6e35a2016af576f4ad8a386668aeb594fcc
Jonas Schneider <mail@jonasschneider.com>
2015-03-21 17:07:52 +0100

    Send some actually meaningful data over the downlink.

--- controller/downlink.cpp
+++ controller/downlink.cpp
@@ -1,13 +1,25 @@
 #include <Arduino.h>
 #include <stdint.h>
 #include "virtual_wire.h"
+#include <avr/io.h>
+
+#include "Arduino.h"
+#include "config.h"
+#include "def.h"
+#include "types.h"
+#include "MultiWii.h"
 
 void mySerialCom() {
   if(vx_tx_active()) {
     // transmitter is still sending, handle that somehow?
   } else {
-    const char *msg = "Hello!";
+    uint16_t msg[4];
+    msg[0] = (uint16_t)(f.ARMED ? '!' : '-');
+    msg[1] = imu.accSmooth[0];
+    msg[2] = imu.accSmooth[1];
+    msg[3] = imu.accSmooth[2];
+
     // we're done sending, shoot the next one
-    vw_send((uint8_t*)msg, strlen(msg));
+    vw_send((uint8_t*)msg, sizeof(msg));
   }
 }
